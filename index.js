b75e22b50a39806173f3d43db65b978661acbbe9
vatako <vatakoemail@bk.ru>
2026-05-19 09:50:25 +0300

    редакт

--- index.js
+++ index.js
@@ -2,21 +2,21 @@ const defaultMovies = [
     {
         title: "Линчеватель",
         year: "2023",
-        status: "Оценено+",
+        status: "СМОТРЮ",
         rating: "★★★★★",
         img: "https://0.soompi.io/wp-content/uploads/2023/10/16022854/vigilante-1.jpg"
     },
     {
         title: "Дьявол носит Prada",
         year: "2006",
-        status: "Оценено",
+        status: "ОЦЕНЕНО",
         rating: "★★★★★",
         img: "https://avatars.mds.yandex.net/get-kinopoisk-image/1900788/61e6f0e1-760d-4ff3-b434-beab6275c0bc/1920x"
     },
     {
         title: "Зверополис",
         year: "2016",
-        status: "Оценено",
+        status: "ОЦЕНЕНО",
         rating: "★★★★★",
         img: "https://basket-05.wbbasket.ru/vol776/part77693/77693884/images/big/1.webp"
     }
@@ -28,7 +28,7 @@ function renderMovies() {
 
     list.innerHTML = defaultMovies.map(movie => `
         <div class="movie-card">
-            <div class="poster-wrapper"><img src="${movie.img}"></div>
+            <div class="poster-wrapper"><img src="${movie.img}" onerror="this.src='placeholder.jpg'"></div>
             <div class="card-info">
                 <h3>${movie.title}</h3>
                 <p style="font-size: 0.8rem; color: #a0a0a0;">${movie.year} <span class="status-badge">${movie.status}</span></p>
@@ -38,4 +38,13 @@ function renderMovies() {
     `).join('');
 }
 
+function addMovie(newMovie) {
+    defaultMovies.push(newMovie);
+    localStorage.setItem('movies', JSON.stringify(defaultMovies));
+    renderMovies();
+}
+
 document.addEventListener('DOMContentLoaded', renderMovies);
+
+
+
